52eda5909ddd76b448c42b974b305249237f8cad
Nitin Minhas <nitin.minhas@hotmail.com>
2024-07-16 13:23:49 -0400

    pushing assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
+    <>
+    <Navbar />
     <div className="h-screen flex justify-center items-center">
       <h1 className="text-5xl text-darkTeal">LVM Counselling</h1>
     </div>
+    </>
+
   );
 }
 
