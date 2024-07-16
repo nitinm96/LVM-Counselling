900ada7db0ff0b33cc9e03730da49838ccb52eb1
nitinm96 <nitin.minhas@hotmail.com>
2024-07-16 13:15:18 -0400

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <h1 className="text-5xl text-darkTeal">LVM Counselling</h1>
+    </div>
+  );
+}
+
+export default App;
