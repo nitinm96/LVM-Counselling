0b86d09e75651dc0c7a2b594893634b0e25136e3
nitinm96 <nitin.minhas@hotmail.com>
2024-07-18 13:40:19 -0400

    adding framer motion

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import homeImg1 from "../assets/slicedPhotos/images/home_img_01.jpg";
 import homeImg2 from "../assets/slicedPhotos/images/home_img_02.jpg";
 import homeImg3 from "../assets/slicedPhotos/images/home_img_03.jpg";
+import { motion } from "framer-motion";
 
 function Home() {
   return (
@@ -18,15 +19,49 @@ function Home() {
           href="https://www.linkedin.com/in/lavanya-verma-aa168116a/"
           target="_blank"
         >
-          <button className="bg-mdWhite text-darkTeal text-xl m-4 px-8 py-2 rounded-lg hover:bg-darkTeal hover:text-mdWhite transition-all duration-150 ease-out cursor-pointer ">
+          <motion.button
+            className="bg-mdWhite text-darkTeal text-xl m-4 px-8 py-2 rounded-lg hover:bg-darkTeal hover:text-mdWhite transition-all duration-150 ease-out cursor-pointer"
+            initial={{ y: "-5%", opacity: 0 }}
+            animate={{ y1: 0, opacity: 1 }}
+            transition={{ duration: 0.5, delay: 2 }}
+          >
             Start Your Journey
-          </button>
+          </motion.button>
         </a>
-        <div className="flex flex-row justify-center items-center gap-x-7">
-          <img src={homeImg1} alt="home" className=" rounded-tl-[25%]" />
-          <img src={homeImg2} alt="home" />
-          <img src={homeImg3} alt="home" className=" rounded-tr-[25%]" />
+        <div className="hidden md:flex md:flex-row md:justify-center md:items-center md:gap-x-7">
+          <motion.img
+            src={homeImg1}
+            alt="home"
+            className=" rounded-tl-[25%] h-4/5"
+            initial={{ x: "-5%", opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.5, delay: 0.5 }}
+          />
+          <motion.img
+            src={homeImg2}
+            alt="home"
+            className="h-4/5"
+            initial={{ x: "-5%", opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.5, delay: 1 }}
+          />
+          <motion.img
+            src={homeImg3}
+            alt="home"
+            className=" rounded-tr-[25%] h-4/5"
+            initial={{ x: "-5%", opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.5, delay: 1.5 }}
+          />
         </div>
+        <motion.div
+          className="md:hidden p-6 bg-darkBrown/25"
+          initial={{ x: "-5%", opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.5 }}
+        >
+          <img src={homeImg1} alt="home" className="" />
+        </motion.div>
       </div>
 
       {/* <div className="h-screen flex flex-col justify-center items-center bg-home-bg">
